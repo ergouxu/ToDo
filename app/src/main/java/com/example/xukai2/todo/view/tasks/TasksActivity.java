b80b7c863cc7661ba4d0c94c69694ecca86dbc86
ergouxu55@gmail.com <ergouxu55@gmail.com>
2019-03-18 09:02:01 +0800

    新做关于task的代码

--- app/src/main/java/com/example/xukai2/todo/view/tasks/TasksActivity.java
+++ app/src/main/java/com/example/xukai2/todo/view/tasks/TasksActivity.java
@@ -1,15 +1,89 @@
 package com.example.xukai2.todo.view.tasks;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
+import com.example.xukai2.todo.Injection;
 import com.example.xukai2.todo.R;
+import com.example.xukai2.todo.presenter.tasks.TasksFilterType;
+import com.example.xukai2.todo.presenter.tasks.TasksPresenter;
+import com.example.xukai2.todo.utils.ActivityUtils;
+import com.example.xukai2.todo.view.statistics.StatisticsActivity;
 
 public class TasksActivity extends AppCompatActivity {
+
+    private static final String CURRENT_FILTERING_KEY = "CURRENT_FILTERING_KEY";
+
+    private DrawerLayout mDrawerLayout;
+
+    private TasksPresenter mTasksPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
+
+        //Set up the toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayShowHomeEnabled(true);
+
+        // Set up the navigation drawer
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerLayout.setStatusBarBackground(R.color.colorPrimary);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            setupDrawerContent(navigationView);
+        }
+
+        TasksFragment tasksFragment = (TasksFragment) getSupportFragmentManager().findFragmentById(R.id.contentFrame);
+        if (tasksFragment == null){
+            // Create the fragment
+            tasksFragment = TasksFragment.newInstance();
+            ActivityUtils.addFragmentToActivity(getSupportFragmentManager(), tasksFragment, R.id.contentFrame);
+        }
+
+        // Create the presenter
+        mTasksPresenter = new TasksPresenter(Injection.provideTasksRepository(getApplicationContext()), tasksFragment);
+
+        // Load previously saved state, if available
+        if (savedInstanceState != null){
+            TasksFilterType currentFiltering = (TasksFilterType) savedInstanceState.getSerializable
+                    (CURRENT_FILTERING_KEY);
+            mTasksPresenter.setFiltering(currentFiltering);
+        }
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.list_navigation_menu_item:
+                        //Do nothing, we're already no that screen
+                        break;
+                    case R.id.statistics_navigation_menu_item:
+                        Intent intent = new Intent(TasksActivity.this, StatisticsActivity.class);
+                        startActivity(intent);
+                        break;
+                    default:
+                        break;
+                }
+                //Close the navigation drawer when an item is selected
+                item.setChecked(true);
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
     }
 }
--- app/src/main/java/com/example/xukai2/todo/view/tasks/TasksFragment.java
+++ app/src/main/java/com/example/xukai2/todo/view/tasks/TasksFragment.java
@@ -1,110 +1,222 @@
 package com.example.xukai2.todo.view.tasks;
 
-import android.content.Context;
-import android.net.Uri;
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.xukai2.todo.R;
+import com.example.xukai2.todo.data.Task;
+import com.example.xukai2.todo.presenter.tasks.TasksContract;
+
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link TasksFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link TasksFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class TasksFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class TasksFragment extends Fragment implements TasksContract.View {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
+    private TasksContract.Presenter mPresenter;
+    private TasksAdapter mListAdapter;
+    private View mNoTaskView;
+    private ImageView mNoTaskIcon;
+    private TextView mNOTaskMainView;
+    private TextView mNOTaskAddView;
+    private LinearLayout mTaskView;
+    private TextView mFilteringLabelView;
 
     public TasksFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TasksFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TasksFragment newInstance(String param1, String param2) {
-        TasksFragment fragment = new TasksFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static TasksFragment getInstance(){
+        return new TasksFragment();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public void setLoadingIndicator(boolean active) {
+
+    }
+
+    @Override
+    public void showTasks(List<Task> tasks) {
+
+    }
+
+    @Override
+    public void showAddTask() {
+
+    }
+
+    @Override
+    public void showTaskDetailsUi(String taskId) {
+
+    }
+
+    @Override
+    public void showTaskMarkedCompleted() {
+
+    }
+
+    @Override
+    public void shoeTaskMarkedActive() {
+
+    }
+
+    @Override
+    public void showCompletedTasksCleared() {
+
+    }
+
+    @Override
+    public void showLoadingTasksError() {
+
+    }
+
+    @Override
+    public void showNoTasks() {
+
+    }
+
+    @Override
+    public void showActiveFilterLabel() {
+
+    }
+
+    @Override
+    public void showCompletedFilterLabel() {
+
+    }
+
+    @Override
+    public void showAllFilterLabel() {
+
+    }
+
+    @Override
+    public void showNoActiveTasks() {
+
+    }
+
+    @Override
+    public void showNoCompletedTasks() {
+
+    }
+
+    @Override
+    public void showSuccessfullySaveMessage() {
+
+    }
+
+    @Override
+    public void isActive() {
+
+    }
+
+    @Override
+    public void showFilteringPopupMenu() {
+
+    }
+
+    @Override
+    public void setPresenter(TasksContract.Presenter presenter) {
+
+    }
+
+    private static class TasksAdapter extends BaseAdapter {
+
+        private List<Task> mTasks;
+        private TaskItemListener mItemListener;
+
+        public TasksAdapter(List<Task> tasks, TaskItemListener itemListener) {
+            setList(tasks);
+            this.mItemListener = itemListener;
         }
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tasks, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+        public void replaceData(List<Task> tasks, TaskItemListener itemListener) {
+            setList(tasks);
+            notifyDataSetChanged();
         }
-    }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+        private void setList(List<Task> tasks) {
+            mTasks = checkNotNull(tasks);
         }
+
+        @Override
+        public int getCount() {
+            return mTasks.size();
+        }
+
+        @Override
+        public Task getItem(int position) {
+            return mTasks.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup parent) {
+            View rowView = view;
+            if (rowView == null) {
+                LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+                rowView = inflater.inflate(R.layout.task_item, parent, false);
+            }
+
+            final Task task = getItem(position);
+
+            TextView titleTV = (TextView) rowView.findViewById(R.id.title);
+            titleTV.setText(task.getTitleForList());
+
+            CheckBox completedCB = (CheckBox) rowView.findViewById(R.id.complete);
+
+            // Active/completed task UI
+            completedCB.setChecked(task.isCompleted());
+            if (task.isCompleted()) {
+                rowView.setBackgroundDrawable(parent.getContext()
+                        .getResources().getDrawable(R.drawable.list_completed_touch_feedback));
+            } else {
+                rowView.setBackgroundDrawable(parent.getContext()
+                        .getResources().getDrawable(R.drawable.touch_feedback));
+            }
+
+            completedCB.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (!task.isCompleted()) {
+                        mItemListener.onCompleteTaskClick(task);
+                    } else {
+                        mItemListener.onActivateTaskClick(task);
+                    }
+                }
+            });
+
+            rowView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mItemListener.onTaskClick(task);
+                }
+            });
+
+            return rowView;
+        }
+
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
+    private interface TaskItemListener {
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        void onTaskClick(Task clickedTask);
+
+        void onCompleteTaskClick(Task completedTask);
+
+        void onActivateTaskClick(Task activatedTask);
+
     }
 }
